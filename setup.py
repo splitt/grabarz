12775e8fe88cfba8ecec3f6683719a379aefb94c
mzajonz <mzajonz@mzajonz>
2010-08-26 18:28:11 +0200

    initial project

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='Grabarz',
+    version='1.0',
+    long_description=__doc__,
+    packages=['grabarz'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Flask-SQLAlchemy',
+        'pyquery',
+        'dateutils',
+        'celery',
+    ]
+)
